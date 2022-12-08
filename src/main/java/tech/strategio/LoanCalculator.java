6467061319ec8e70c2f76140b678b9e4c43f218f
Andrewcodes12 <andrewfava@yahoo.com>
2022-12-08 13:55:07 -0500

    loan calculator done

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class LoanCalculator {
 
     /**
-     * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
+     *
+     * We've taken a loan from a friend and need to calculate how much we'll owe them after 3 months.
+     * We are paying back 10% of the remaining loan amount each month.
+     * @param amount - an integer for the amount we borrowed from our friend.
+     * @return an integer for the amount we owe our friend after 3 months.
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
+        int count = 0;
+        while (count < 3) {
+            amount = amount - (amount / 10);
+            count++;
+        }
+        return amount;
     }
 
     public static void main(String[] args) {
